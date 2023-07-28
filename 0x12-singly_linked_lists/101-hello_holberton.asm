d52cba6cd2c1f097c4e4af87e1bf6430d52d1cec
254Nicole-Nase <nicole.nasengo@strathmore.edu>
2023-07-28 16:52:01 -0700

    asm edit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,17 @@
 section .data
     hello_message db "Hello, Holberton", 0
-    format db "%s", 0
+    fmt db "%s", 10, 0
 
 section .text
     global main
     extern printf
 
 main:
-    ; Prepare the arguments for printf
-    mov rdi, format
-    mov rsi, hello_message
-
-    ; Call printf
-    call printf
-
-    ; Exit the program
-    mov rdi, 0
-    call exit
-
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
